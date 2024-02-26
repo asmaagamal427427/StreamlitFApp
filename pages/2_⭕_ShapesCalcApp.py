c56bec86bdafc9740f65f145015e65d6cd62d054
Asmaa Gamal <53995061+asmaagamal427427@users.noreply.github.com>
2024-02-26 23:39:09 +0200

    matplotlib error resolved

--- pages/2_⭕_ShapesCalcApp.py
+++ pages/2_⭕_ShapesCalcApp.py
@@ -4,7 +4,6 @@ from random import randint
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 import time
